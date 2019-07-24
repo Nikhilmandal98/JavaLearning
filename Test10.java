19bdf7cb8270ef3fb148060de2556460bc137c68
Nikhilmandal98 <53278614+Nikhilmandal98@users.noreply.github.com>
2019-07-24 17:15:04 -0400

    Create Test10.java

---
+++ Test10.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Test10 {
+
+	public static void main(String[] args) {
+
+		String firstName = "Nirjhor";
+		String middleName = " kumar";
+		String LastName = " mandal";
+		String fullName = firstName + middleName + LastName;
+		System.out.println("fullName: " + fullName);
+
+	}
+}
